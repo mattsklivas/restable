e3144eecf55319ace40e8d89035648c411e86891
hamza <hamzasyedshah2018@outlook.com>
2020-10-27 19:34:35 -0400

    Example of linechart on Result Activity

--- app/src/main/java/com/example/restable/ResultsActivity.java
+++ app/src/main/java/com/example/restable/ResultsActivity.java
@@ -1,14 +1,33 @@
 package com.example.restable;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IFillFormatter;
+import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.Utils;
 
 import java.util.ArrayList;
 
 public class ResultsActivity extends AppCompatActivity {
 
+    private static final String TAG = "ResultsActivity";
+    private LineChart mchart;
     ArrayList<Float> humidityData;
     ArrayList<Float> tempData;
     ArrayList<Float> soundData;
@@ -29,5 +48,28 @@ public class ResultsActivity extends AppCompatActivity {
         System.out.println("humidityData:" + humidityData);
         System.out.println("soundData:" + soundData);
         System.out.println("motionData:" + motionData);
+
+        mchart = (LineChart) findViewById(R.id.linechart);
+//        mchart.setOnChartGestureListener(ResultsActivity.this);
+//        mchart.setOnChartValueSelectedListener(ResultsActivity.this);
+        mchart.setDragEnabled(true);
+        mchart.setScaleEnabled(false);
+
+        ArrayList<Entry> yValues = new ArrayList<>();
+
+        yValues.add(new Entry(0, 60f));
+        yValues.add(new Entry(1, 60f));
+
+        LineDataSet set1 = new LineDataSet(yValues, "Humidity Data Set 1");
+
+        set1.setFillAlpha(110);
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1);
+
+        LineData data = new LineData(dataSets);
+
+        mchart.setData(data);
+
     }
 }
